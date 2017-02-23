d36f493b69eace21fcb82a1b9d81b1019fca7bdd
bogdan <poznyak.pba@gmail.com>
2017-02-24 00:21:02 +0300

    all gems installed, and omniauth is ok!

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  
-  get 'home/index'
-  get '/', to: 'home#index'
+    
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
