ac90cbc8c64968b77889888d7d132e13c8aceb77
bogdan <poznyak.pba@gmail.com>
2017-03-10 00:49:22 +0300

    refactoring views of all models(creating partials)

--- app/assets/javascripts/instructions.js
+++ app/assets/javascripts/instructions.js
@@ -1,3 +1,20 @@
 $(document).on('turbolinks:load', function() {
 	$( "#instruction_category_id" ).selectmenu();
+
+	$('#instruction_tag_list').tagsInput({
+    'onAddTag': addTagsToInput, 
+    'onRemoveTag': addTagsToInput, 
+  });
+	
+	
+	function addTagsToInput(e) {
+		var tags = $('#instruction_tag_list_tagsinput>span.tag>span');
+		console.log(tags);
+		var tagsText = [];
+		$(tags).each(function () {
+	  	tagsText.push($(this).text().replace(/..$/,''));
+		});
+		$('#instruction_tag_list').attr('value', tagsText.join(','));
+	}
+
 });
